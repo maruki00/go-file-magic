120eef7736015628fd6afda271633a5089b5f097
maruki00 <aitbaha48@gmail.com>
2025-09-27 14:38:16 +0100

    Update type.go

--- type.go
+++ type.go
@@ -1,1 +1,12 @@
-package 
+package github.com/maruki00/go-magic
+
+
+type FileType struct {
+	Description string
+	Ext string
+	Offset uint8
+}
+
+var Magics = map[[]byte]FileType {
+	[]byte{} : FileType{}
+}
